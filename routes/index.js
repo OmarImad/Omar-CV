1ccb47e73ecb4e3cd0867a8fc53f33e2e9dc985c
unknown <omar.imad.ali.99@gmail.com>
2021-09-25 12:36:29 +0300

    email

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var router = express.Router();
-const sgMail = require('@sendgrid/mail')
 const nodemailer = require("nodemailer");
 
 /* GET home page. */
@@ -19,23 +18,29 @@ router.post("/send", (req, res) => {
               <h3>Name: ${req.body.name}</h3>
               <p>${req.body.msg}</p>
   `
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const msg = {
-  to: 'fack21gasa@gmail.com', // Change to your recipient
-  from: 'omar.imad.ali.99@gmail.com', // Change to your verified sender
-  subject: subject,
-  text: message,
-  html: other,
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
-
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'extra1store@gmail.com',
+      pass: '12345!@#$%'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'exstra1store@gmail.com',
+    to: 'omar.imad.ali.99@gmail.com',
+    subject: subject,
+    text: 'Anything',
+    html: other
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   res.send('<h2>Your message has been sent. Thank you! </h2><br /><a href="/"><div class="text-center"><button type="submit">Back</button></div></a>')
     // res.redirect("/");
 });
